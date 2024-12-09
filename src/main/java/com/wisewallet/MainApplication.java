9bf05f8210319e7287a7c2d39a06817df06f5424
Carlos Henrique Silva Lopes <luiscarloslopes597@gmail.com>
2024-12-09 14:12:57 -0300

    Att

--- src/main/java/com/wisewallet/MainApplication.java
+++ src/main/java/com/wisewallet/MainApplication.java
@@ -10,7 +10,8 @@ public class MainApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/login.fxml"));
         Scene scene = new Scene(loader.load());
-        primaryStage.setTitle("Smart Wallet - Login");
+        primaryStage.setTitle("Login - Controle de Finanças");
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
